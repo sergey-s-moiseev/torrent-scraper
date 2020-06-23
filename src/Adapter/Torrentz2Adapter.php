5a6169ba71f5a2e993ce101d3732e100197107e5
Sergey S. Moiseev <sergey@moses-team.ru>
2020-06-23 17:29:02 +0300

    Update Tz2 crawler

--- src/Adapter/Torrentz2Adapter.php
+++ src/Adapter/Torrentz2Adapter.php
@@ -19,6 +19,7 @@ class Torrentz2Adapter implements AdapterInterface
   use LoggerAware;
 
   const ADAPTER_NAME = 'torrentz2';
+  const SHUB_KEY = '';
 
   /**
    * @var array
@@ -30,19 +31,19 @@ class Torrentz2Adapter implements AdapterInterface
    */
   public function __construct(array $options = [])
   {
-    $this->options = array_merge(
-      [
-        'node_path' => null,
-        'node_modules_path' => null
-      ],
-      array_filter(
-        $options,
-        function($key){
-          return in_array($key, ['node_path', 'node_modules_path']);
-        },
-        ARRAY_FILTER_USE_KEY
-      )
-    );
+//    $this->options = array_merge(
+//      [
+//        'node_path' => null,
+//        'node_modules_path' => null
+//      ],
+//      array_filter(
+//        $options,
+//        function($key){
+//          return in_array($key, ['node_path', 'node_modules_path']);
+//        },
+//        ARRAY_FILTER_USE_KEY
+//      )
+//    );
   }
 
   /**
@@ -58,7 +59,7 @@ class Torrentz2Adapter implements AdapterInterface
    */
   public function getUrl()
   {
-    return 'https://torrentz2.eu/';
+    return 'https://torrentz.unblockit.pw/';
   }
 
   /**
@@ -66,165 +67,41 @@ class Torrentz2Adapter implements AdapterInterface
    */
   public function search($query='')
   {
-//     $cookieFile = tmpfile();
-//
-//     $client = new Client([
-//       // 'debug' => true,
-//       'cookies' => new FileCookieJar($this->getTmpFilename($cookieFile)),
-//       'headers' => [ // these headers need to avoid recaptcha request
-//         'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-//         'Accept-Encoding' => 'gzip, deflate',
-//         'Accept-Language' => 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
-//       ]
-//     ]);
-//     $client->getConfig('handler')->push(CloudflareMiddleware::create($this->options['node_path'], $this->options['node_modules_path']));
-//
-//     $urls = empty($query) ?
-//       [
-//         [
-//           'video' => 'https://torrentz2.eu/searchA?f=movies%20added%3A3d',
-//           'video tv' => 'https://torrentz2.eu/searchA?f=tv%20added%3A9d',
-//           'music' => 'https://torrentz2.eu/searchA?f=music%20added%3A30d',
-//           'game' => 'https://torrentz2.eu/searchA?f=games%20added%3A40d'
-//         ],
-//         [
-//           'video' => 'https://torrentz2.eu/search?f=movies%20added%3A3d',
-//           'video tv' => 'https://torrentz2.eu/search?f=tv%20added%3A9d',
-//           'music' => 'https://torrentz2.eu/search?f=music%20added%3A30d',
-//           'game' => 'https://torrentz2.eu/search?f=games%20added%3A40d'
-//         ]
-//       ] :
-//       [
-//         [sprintf('https://torrentz2.eu/searchA?f=%s/', urlencode($query))],
-//         [sprintf('https://torrentz2.eu/search?f=%s/', urlencode($query))]
-//       ]
-//     ;
-//
-//     $response = array_map(
-//       function($urlSet) use($client)
-//       {
-//         return array_filter(array_map(
-//           function($url) use($client)
-//           {
-//             try{
-//               return $client->request('GET', $url);
-//             } catch (\Exception $e) {
-//               $this->log(\Psr\Log\LogLevel::ERROR, $e->getMessage());
-//               // var_dump($e->getMessage());
-//               return [];
-//             }
-//           },
-//           $urlSet
-//         ));
-//       },
-//       $urls
-//     );
-//
-//     fclose($cookieFile);
+      $project = 0;
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, "https://app.scrapinghub.com/api/jobs/list.json?project=$project&spider=tz2&state=finished&count=1");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_USERNAME, self::SHUB_KEY);
+      $output = curl_exec($ch);
+      curl_close($ch);
+      $job_info = json_decode($output);
+      $job_id = $job_info->jobs[0]->id;
 
-    $results = [];
-//     $hashes = [];
-//     $category = null;
-//     $now = new DateTime();
-//
-//     foreach ($response as $_response) {
-//       foreach ($_response as $category => $__response) {
-//         $name = '';
-//         $hash = '';
-//         $magnet = '';
-//         $seeders = 0;
-//         $leechers = 0;
-//         $size = 0;
-//         $age = $now;
-//
-//         $crawler = new Crawler((string)$__response ->getBody());
-//         $items = $crawler->filter('div.results')->filter('dl');
-//
-//         foreach ($items as $item) {
-//           $itemCrawler = new Crawler($item);
-//
-//           try {
-//             /**Name**/
-//             $name = $itemCrawler->filter('a')->text();
-//             /**Hash**/
-//             $hash = $itemCrawler->filter('a')->attr('href');
-//             $hash = strtolower($hash);
-//             if ($hash) $hash = substr($hash, 1);
-//             $magnet = 'magnet:?xt=urn:btih:' . $hash . '&dn=' . $name;
-//
-//             /**Validate hash **/
-//             if(!(preg_match("/^[a-f0-9]{40}$/",$hash))){continue;}
-//
-//             /**Size**/
-//             $size = $itemCrawler->filter('dd')->filter('span:nth-child(3)')->text();
-//             preg_match("/MB|GB|TB|KB/", $size, $k_size);
-//             preg_match("/[0-9]+(\S[0-9]+)?/", $size, $size);
-//             $size = (float)$size[0];
-//             switch ($k_size[0]) {
-//               case 'KB':
-//                 $size = $size * 1 / 1024;
-//                 break;
-//               case 'MB':
-//                 break;
-//               case 'GB':
-//                 $size = $size * 1024;
-//                 break;
-//               case 'TB':
-//                 $size = $size * 1024 * 1024;
-//                 break;
-//             }
-//
-//             /**Category**/
-//             if ($query) {
-//               $input_line = $itemCrawler->filter('dt')->text();
-//               preg_match("/» (.+)/", $input_line, $output_line);
-//               $input_line = $output_line[1];
-//               $input_line == 'video tv' ? $output_line[0] = 'video tv' : preg_match("/^\S+/", $input_line, $output_line);
-//               $category = $output_line[0];
-//             }
-//           } catch (\Exception $e) {
-//             continue;
-//           }
-//
-//           /**Age**/
-//           try {
-//             $age = $itemCrawler->filter('dd')->filter('span:nth-child(2)')->attr('title');
-//           } catch (\Exception $e) {}
-//           /**Seeders**/
-//           try{
-//             $seeders = (int)str_replace([',', '.'], '', $itemCrawler->filter('dd')->filter('span:nth-child(4)')->text());
-//           } catch (\Exception $e) {}
-//
-//           /**Leechers**/
-//           try{
-//             $leechers = (int)str_replace([',', '.'], '', $itemCrawler->filter('dd')->filter('span:nth-child(5)')->text());;
-//           } catch (\Exception $e) {}
-//
-//           /**Verified**/
-//           try{
-//             $verified = 0 < strlen(preg_replace('/\s/', '', $itemCrawler->filter('dd')->filter('span:nth-child(1)')->text()));
-//           } catch (\Exception $e) {}
-//
-//           if (in_array($hash, $hashes) == false) {
-//             $result = new SearchResult();
-//             $result->setName($name)
-//               ->setCategory($category)
-//               ->setDetailsUrl('https://torrentz2.eu/' . $hash)
-//               ->setSource(self::ADAPTER_NAME)
-//               ->setSeeders($seeders)
-//               ->setLeechers($leechers)
-//               ->setSize($size)
-//               ->setMagnetUrl($magnet)
-//               ->setTimestamp($age)
-//               ->setIsVerified($verified)
-//             ;
-//             $results[] = $result;
-//             $hashes[] = $hash;
-//           }
-//         }
-//       }
-//     }
-//     echo "\n T2 - completed. ".count($results)." crawled\n";
+      $url = "https://storage.scrapinghub.com/items/$job_id?format=json";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_USERNAME, self::SHUB_KEY);
+      $output = curl_exec($ch);
+      curl_close($ch);
+      $items = json_decode($output);
+      $results = [];
+
+      foreach ($items as $item) {
+          $result = new SearchResult();
+          $result->setCategory($item->categories[0]);
+          $result->setName($item->title);
+          $result->setDetailsUrl($this->getUrl().$item->url);
+          $result->setSeeders((int) $item->seeds);
+          $result->setLeechers($item->peers);
+          $result->setTimestamp((int) $item->released);
+          $result->setSource(self::ADAPTER_NAME);
+          $result->setMagnetUrl($item->magnet);
+          $result->setSize($item->size);
+          $result->setIsVerified($item->verified);
+          $results[] = $result;
+      }
+     echo "\n T2 - completed. ".count($results)." crawled\n";
     return $results;
   }
 
